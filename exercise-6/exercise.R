82869fd2875edd3e53d524e72036e424f4e54bdb
evanfrawley <frawley@uw.edu>
2017-01-17 08:44:24 -0800

    Add putting vectors into DF and writing DF to csv

--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -2,25 +2,41 @@
 # Read in the Husky Football 2015 game data into a variable called `husky.games.2015`
 
 
+
 # Create a vector of the teams that the Huskies played against during that season
+
 
 
 # Create a vector of the their final scores for the games
 # Call this variable `husky.scores`
 
 
+
 # Create 2 variables called `rushing.yards` and `passing.yards` to represent the yards the Huskies rushed and passed
+
 
 
 # Create a variable called `combined.yards` that is the total yardage of the Huskies for each game
 
 
+
+# Add the `combined.yards` vector to the dataframe
+
+
+
+# Write the dataframe to a new `.csv` file called `huskies_2015_improved.csv`
+
+
+
 # What is the score of the game where the Huskies had the most combined yards?
+
 
 
 # Define a function `MostYardsScore` that takes in an argument `games` (a data frame) and
 # returns a descriptive sentence about the game that was played that had the most yards scored in it.
 
 
+
+# Challenge!
 # What was the highest yardage game so far this season?
 # Hint: Read in the dataset titled `huskies_2016.csv` and save it as a variable
