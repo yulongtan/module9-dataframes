a44b027f68cc99c44488ac9c540784124bffac48
Yulong Tan <yulongtan67@gmail.com>
2017-01-18 16:31:01 -0800

    complete exercise 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,49 +1,50 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
 # Hint: use the `paste()` function to produce the list
-
+employees <- paste("Employee", 1:100)
 
 # Create a vector of 100 random salaries for the year 2014
 # Use the `runif()` function to pick a random number between 40000 and 50000
-
+salaries.2014 <- c(runif(100, 40000, 50000))
 
 # Create a vector of 100 salaries in 2015 that have increased from 2014 by some amount
 # Hint: use `runif()` to add a random number to 2014's salaries. Starting from a
 # _negative_ number so that salaries may decrease!
-
+salaries.2015 <- salaries.2014 + c(runif(100, -5000, 10000))
 
 # Create a data.frame 'salaries' by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-
+salaries <- data.frame(employees, salaries.2014, salaries.2015, stringsAsFactors=FALSE)
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+salaries$raise <- salaries.2015 - salaries.2014
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+salaries$got.raise <- salaries$raise > 0
 
 ### Retrieve values from your data frame to answer the following questions
 ### Note that you should get the value as specific as possible (e.g., a single
 ### cell rather than the whole row!)
+View(salaries)
 
 # What was the 2015 salary of employee 57
-
+salaries[salaries$employees == "Employee 57", "salaries.2015"]
 
 # How many employees got a raise?
-
+nrow(salaries[salaries$got.raise == TRUE, ])
 
 # What was the value of the highest raise?
-
+max.salary <- max(salaries$raise)
 
 # What was the "name" of the employee who received the highest raise?
-
+print(salaries[salaries$raise == max.salary, "employees"])
 
 # What was the largest decrease in salaries between the two years?
-
+min.salary <- min(salaries$raise)
 
 # What was the name of the employee who recieved largest decrease in salary?
-
+print(salaries[salaries$raise == min.salary, "employees"])
 # What was the average salary increase?
-
+print(mean(salaries$raise))
 
 ### Bonus ###
 
